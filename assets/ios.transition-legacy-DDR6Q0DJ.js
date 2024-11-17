62a7c293291db8c3e0dca2b45ea742a5ef9ac66d
Denis Yakorev <denisyakorev@mail.ru>
2024-11-17 21:02:23 +0300

    feature: change notes import

--- assets/ios.transition-legacy-DDR6Q0DJ.js
+++ assets/ios.transition-legacy-DDR6Q0DJ.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-YhXYbfFz.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.n}],execute:function(){
+System.register(["./index-legacy-BLbLgoYB.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
