95ce77f43a396a0851a0570a7bd6505fc05fba2a
Denis Yakorev <denisyakorev@mail.ru>
2024-11-22 11:39:51 +0300

    feature: move to antd

--- assets/ios.transition-legacy-gfDLg6l5.js
+++ assets/ios.transition-legacy-gfDLg6l5.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BmBfLfM7.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.a,n=t.g}],execute:function(){
+System.register(["./index-legacy-CAzMWefg.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.a,n=t.g}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
